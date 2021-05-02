1e7c17e0e1c05a9ef34403ae317e01afa2f78439
Marc-Olivier Lalonde <marco.pb28@hotmail.com>
2021-05-02 17:27:31 -0400

    Fix collision et ajout pointeur fleche coffre 2d

--- detectionObjetAutourJoueur.js
+++ detectionObjetAutourJoueur.js
@@ -5,27 +5,10 @@ function detectionObjetAutourJoueur() {
   fltX = getCibleCameraX(objScene3D.camera) - getPositionCameraX(objScene3D.camera);
   fltZ = getCibleCameraZ(objScene3D.camera) - getPositionCameraZ(objScene3D.camera);
   fltRayon = Math.sqrt(fltX * fltX + fltZ * fltZ);
-
-  // Si primeX = 0 && primeZ = 1 -> Nord
-  // Si primeX = 0 && primeZ = -1 -> Sud
-  // Si primeX = -1 && primeZ = 0 -> Est
-  // Si primeX = 1 && primeZ = 0 -> Ouest
-  fltXPrime = 0.25 * Math.cos(Math.acos(fltX / fltRayon));
-  fltZPrime = 0.25 * Math.sin(Math.asin(fltZ / fltRayon));
+  fltXPrime = 0.2 * Math.cos(Math.acos(fltX / fltRayon));
+  fltZPrime = 0.2 * Math.sin(Math.asin(fltZ / fltRayon));
   fltXPrimeFar = 1 * Math.cos(Math.acos(fltX / fltRayon));
   fltZPrimeFar = 1 * Math.sin(Math.asin(fltZ / fltRayon));
-
-
-  //document.getElementById('primeCamX').innerHTML = "primeX : " + fltXPrime;
-  //document.getElementById('primeCamZ').innerHTML = "primeZ : " + fltZPrime;
-
-  // Pour savoir dans quelle direction quon regarde
-  let strDirection = "";
-  if (fltZPrimeFar > 0.5 && fltZPrimeFar < 1) strDirection = "Sud";
-  else if (fltZPrimeFar < -0.5 && fltZPrimeFar > -1) strDirection = "Nord";
-  else if (fltXPrimeFar > 0.5 && fltXPrimeFar < 1) strDirection = "Est";
-  else if (fltXPrimeFar < -0.5 && fltXPrimeFar > -1) strDirection = "Ouest";
-  //document.getElementById('Direction').innerHTML = "direction: " + strDirection;
 
   objScene3D.camera.objAutourJoueur = {
       objAPosJoueur: {
@@ -53,5 +36,10 @@ function detectionObjetAutourJoueur() {
         intX: Math.floor(intX - fltXPrimeFar * INT_CELL_LARGEUR),
         intZ: Math.floor(intZ - fltZPrimeFar * INT_CELL_PROFONDEUR),
       },
+      objEnAvantDroitJoueur: {
+         strType: tabJeu[Math.floor(intZ+2*fltZPrime)][Math.floor(intX+2*fltXPrime)],
+          intX: Math.floor(intX + 2*fltXPrime * INT_CELL_LARGEUR),
+          intZ: Math.floor(intZ + 2*fltZPrime * INT_CELL_PROFONDEUR),
+      }
   }        
 }
---
+++ flecheTest.js
@@ -0,0 +1,86 @@
+function creerObj3DPointeFleche(objgl, intNoTexture, intX, intZ, binVisible) {
+    const obj3DPointeFleche = new Object();
+    obj3DPointeFleche.fltProfondeur = 0.5;
+    obj3DPointeFleche.fltLargeur = 0.2;
+    obj3DPointeFleche.fltHauteur = 0.01 ;
+    obj3DPointeFleche.binVisible = binVisible;
+    obj3DPointeFleche.intX = intX;
+    obj3DPointeFleche.intZ = intZ;
+
+    obj3DPointeFleche.vertex = creerVertexPointeFleche(objgl, obj3DPointeFleche.fltLargeur, obj3DPointeFleche.fltProfondeur, obj3DPointeFleche.fltHauteur);
+    obj3DPointeFleche.couleurs = creerCouleursPointeFleche(objgl, [1, 0, 0, 1]);
+    obj3DPointeFleche.texels = creerTexelsPointeFlechePosJoueur(objgl, obj3DPointeFleche.fltLargeur, obj3DPointeFleche.fltProfondeur, intNoTexture);
+    obj3DPointeFleche.maillage = creerMaillagePointeFleche(objgl);
+
+    obj3DPointeFleche.transformations = creerTransformations();
+    changerPos(obj3DPointeFleche.intX, obj3DPointeFleche.intZ, obj3DPointeFleche.transformations);
+    let posZcoffre = (intX - tabPosCoffre[intNiveau-1].intZ);
+    let posXcoffre = (intZ - tabPosCoffre[intNiveau-1].intX);
+    let angleRotationFleche = Math.atan(posZcoffre/posXcoffre) / (Math.PI / 180);    
+    if (angleRotationFleche < 0) angleRotationFleche = 180 + angleRotationFleche;
+    document.getElementById("strAv").innerHTML = angleRotationFleche; 
+    
+    setAngleY(angleRotationFleche, obj3DPointeFleche.transformations);
+    return obj3DPointeFleche;
+}
+
+function creerVertexPointeFleche(objgl, fltLargeur, fltProfondeur, fltHauteur) {
+    const tabVertex = [
+        0, fltHauteur, -fltProfondeur / 2,
+        -fltLargeur / 2, fltHauteur, fltProfondeur / 2,
+        fltLargeur / 2, fltHauteur, fltProfondeur / 2,
+    ];
+
+    const objPointeFleche = objgl.createBuffer();
+    objgl.bindBuffer(objgl.ARRAY_BUFFER, objPointeFleche);
+    objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabVertex), objgl.STATIC_DRAW);
+
+    return objPointeFleche;
+}
+
+function creerCouleursPointeFleche(objgl, tabCouleur) {
+    tabCouleurs = [];
+    for (let i = 0; i < 4; i++)
+        tabCouleurs = tabCouleurs.concat(tabCouleur);
+
+    const objCouleursPointeFleche = objgl.createBuffer();
+    objgl.bindBuffer(objgl.ARRAY_BUFFER, objCouleursPointeFleche);
+    objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabCouleurs), objgl.STATIC_DRAW);
+
+    return objCouleursPointeFleche;
+}
+
+function creerTexelsPointeFlechePosJoueur(objgl, fltLargeur, fltProfondeur, intNoTexture) {
+    const tabTexels = [
+        fltLargeur/2, 0.0,
+        0.0, fltProfondeur,
+        fltLargeur, fltProfondeur
+    ];
+
+    const objTexelsPointeFleche = objgl.createBuffer();
+    objgl.bindBuffer(objgl.ARRAY_BUFFER, objTexelsPointeFleche);
+    objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabTexels), objgl.STATIC_DRAW);
+
+    objTexelsPointeFleche.intNoTexture = intNoTexture; objTexelsPointeFleche.pcCouleurTexel = 0.0;
+
+    return objTexelsPointeFleche;
+}
+
+function creerMaillagePointeFleche(objgl) {
+
+    const tabMaillage =
+        [ // Les 2 triangles du PointeFlechePosJoueur
+            0, 1, 2
+        ];
+
+    const objMaillagePointeFleche = objgl.createBuffer();
+    objgl.bindBuffer(objgl.ELEMENT_ARRAY_BUFFER, objMaillagePointeFleche);
+    objgl.bufferData(objgl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tabMaillage), objgl.STATIC_DRAW);
+
+    // Le nombre de triangles
+    objMaillagePointeFleche.intNbTriangles = 1;
+    // Le nombre de droites
+    objMaillagePointeFleche.intNbDroites = 0;
+
+    return objMaillagePointeFleche;
+}
